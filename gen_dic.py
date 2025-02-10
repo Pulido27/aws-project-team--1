f3d75eb63e3714d31885f0d7121cc99c2213cacf
Cristian <cpulido@ucol.mx>
2025-02-10 19:56:29 +0000

    segundo commit

---
+++ gen_dic.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+indices = {}
+
+with open("Titulos","rb") as archivo:
+    pos = archivo.tell() # Obtener la posicion del puntero de lectura
+    
+    char = "a"
+    while char :
+        archivo.seek(pos)
+        linea = archivo.readline().decode("utf-8")
+        data = linea.split('_') 
+        indices[data[0]] = pos
+        pos = archivo.tell()
+        char = archivo.read(1)
+print(indices)
