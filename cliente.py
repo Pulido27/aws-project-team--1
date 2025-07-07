4e6641969fd2e9f43b9f08717c5280f541498e53
Cristian <cpulido@ucol.mx>
2025-07-07 05:46:17 +0000

    añadir al readme el cliente

---
+++ cliente.py
@@ -0,0 +1,34 @@
+import requests
+
+def buscar_libros():
+    url = "http://localhost:8000/search"
+
+    texto = input("¿Qué deseas buscar?: ").strip()
+
+    if not texto:
+        print("No se puede buscar una cadena vacía.")
+        return
+
+    payload = {
+        "query": texto
+    }
+
+    try:
+        response = requests.post(url, json=payload)
+        response.raise_for_status()
+
+        data = response.json()
+        print("\n Resultados:")
+        print(f"Query: {data['query']}")
+        print(f"Total libros encontrados: {data['count']}")
+        for i, libro in enumerate(data['books'], 1):
+            print(f"{i}. {libro}")
+
+    except requests.exceptions.HTTPError as errh:
+        print("Error en la respuesta del servidor:", errh)
+        print("Detalle:", response.text)
+    except requests.exceptions.RequestException as err:
+        print("Error en la conexión:", err)
+
+if __name__ == "__main__":
+    buscar_libros()
